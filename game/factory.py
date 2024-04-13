dbb48ab62ea1e2305758759526569188fdc4be2f
Florian MAILLARD <florian.maillard.thebest@gmail.com>
2024-04-13 23:15:23 +0200

    Improve the class factory

--- game/factory.py
+++ game/factory.py
@@ -1,26 +1,25 @@
-# coding: utf-8
+class Factory:
+    """Class factory to product characters based on string class name."""
 
-class Factory():
-    """Class to act like a pattern factory and product characters based on string class name"""
+    # Dictionary mapping class names to their corresponding classes
+    character_classes = {
+        "warrior": "Warrior",
+        "archer": "Archer",
+        "wizard": "Wizard",
+        "orc": "Orc",
+        "wolf": "Wolf",
+        "zombie": "Zombie",
+    }
 
     @classmethod
-    def get_character(self, class_name):
-        """Methode to return a specific character object based on a class name"""
-        if class_name == "warrior":
-            from characters.warrior import Warrior
-            return Warrior()
-        elif class_name == "archer":
-            from characters.archer import Archer
-            return Archer()
-        elif class_name == "wizzard":
-            from characters.wizzard import Wizzard
-            return Wizzard()
-        elif class_name == "orc":
-            from characters.orc import Orc
-            return Orc()
-        elif class_name == "wolf":
-            from characters.wolf import Wolf
-            return Wolf()
-        elif class_name == "zombie":
-            from characters.zombie import Zombie
-            return Zombie()
+    def get_character(cls, class_name):
+        """Return a specific character object based on a class name."""
+        if class_name in cls.character_classes:
+            # Dynamically import the corresponding class
+            character_class = cls.character_classes[class_name]
+            character_module = __import__(
+                "characters." + character_class.lower(), fromlist=[character_class]
+            )
+            character_class = getattr(character_module, character_class)
+            return character_class()
+        return None
